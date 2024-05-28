3143bf8bd4c10e6b62f02d87225c02221dd25d63
Alex Sandro <alex@creapix.com.br>
2024-05-28 12:56:41 -0300

    Corrigindo o caminho para wite

--- 05 - Manipulação de arquivos/2_operacao_escrita.py
+++ 05 - Manipulação de arquivos/2_operacao_escrita.py
@@ -1,6 +1,6 @@
 arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
+    "D:/DioBootcamp/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
 )
-arquivo.write("Escrevendo dados em um novo arquivo.")
-arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto"])
+arquivo.write("Escrevendo dados em um novo arquivo. Feito por alex Sandro")
+arquivo.writelines(["\n", "escrevendo", "\n", "tudo", "\n", "de novo", "\n", "texto"])
 arquivo.close()
