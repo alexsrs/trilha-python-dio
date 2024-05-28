6dfb33612b4fae58f6df3408a4a20619d48e17cc
Alex Sandro <alex@creapix.com.br>
2024-05-28 11:20:15 -0300

    incluido sem Time zone - UTC

--- 04 - Data e hora/4_pytz.py
+++ 04 - Data e hora/4_pytz.py
@@ -2,8 +2,10 @@ from datetime import datetime
 
 import pytz
 
+data0 = datetime.now(pytz.UTC)
 data = datetime.now(pytz.timezone("Europe/Oslo"))
 data2 = datetime.now(pytz.timezone("America/Sao_Paulo"))
 
 print(data)
 print(data2)
+print(data0)
